0a90ff724d93988a2dac5435c8a2551a5a498582
imamachi-n <imamachigatugatu@hotmail.co.jp>
2019-04-14 20:14:22 +0900

    learning how to typography.js in Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,12 +13,6 @@ module.exports = {
         path: `${__dirname}/src/images`,
       },
     },
-    {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography`,
-      },
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
@@ -31,6 +25,12 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -2,14 +2,52 @@ import Typography from "typography"
 import Wordpress2016 from "typography-theme-wordpress-2016"
 
 Wordpress2016.overrideThemeStyles = () => ({
+  "@font-face": {
+    fontFamily: '"Yu Gothic"',
+    src: 'local("Yu Gothic Medium")',
+    fontWeight: 100,
+  },
+  "@font-face": {
+    fontFamily: '"Yu Gothic"',
+    src: 'local("Yu Gothic Medium")',
+    fontWeight: 200,
+  },
+  "@font-face": {
+    fontFamily: '"Yu Gothic"',
+    src: 'local("Yu Gothic Medium")',
+    fontWeight: 300,
+  },
+  "@font-face": {
+    fontFamily: '"Yu Gothic"',
+    src: 'local("Yu Gothic Medium")',
+    fontWeight: 400,
+  },
+  "@font-face": {
+    fontFamily: '"Yu Gothic"',
+    src: 'local("Yu Gothic Bold")',
+    fontWeight: "bold",
+  },
+  "@font-face": {
+    fontFamily: '"Helvetica Neue"',
+    src: 'local("Helvetica Neue Regular")',
+    fontWeight: 100,
+  },
+  "@font-face": {
+    fontFamily: '"Helvetica Neue"',
+    src: 'local("Helvetica Neue Regular")',
+    fontWeight: 200,
+  },
+
   "html,body,h1,h2,h3,h4,h5,h6": {
     color: "#444",
     fontFamily: [
+      "Roboto",
       "-apple-system-body",
       "BlinkMacSystemFont",
       "Helvetica Neue",
       "Helvetica Sans",
       "Hiragino Kaku Gothic ProN",
+      "Kosugi Maru",
       '"Noto Sans Japanese"',
       '"游ゴシック  Medium"',
       '"Yu Gothic Medium"',

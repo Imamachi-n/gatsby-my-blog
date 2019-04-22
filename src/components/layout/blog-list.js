ffa0f3616a365eee402b3ab8b17459c6dc9b36ec
imamachi-n <imamachigatugatu@hotmail.co.jp>
2019-04-22 21:36:51 +0900

    refactor a blog list section and extract it as blog-list component

---
+++ src/components/layout/blog-list.js
@@ -0,0 +1,62 @@
+import React from "react"
+import { Link, StaticQuery } from "gatsby"
+import { rhythm } from "../../utils/typography"
+import { css } from "@emotion/core"
+
+const Bloglist = () => (
+  <StaticQuery
+    query={graphql`
+      query MarkdownList {
+        allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
+          totalCount
+          edges {
+            node {
+              id
+              frontmatter {
+                title
+                date
+              }
+              fields {
+                slug
+              }
+              excerpt
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <Link
+              to={node.fields.slug}
+              css={css`
+                text-decoration: none;
+                color: inherit;
+              `}
+            >
+              <h3
+                css={css`
+                  margin-bottom: ${rhythm(1 / 4)};
+                `}
+              >
+                {node.frontmatter.title}{" "}
+                <span
+                  css={css`
+                    color: #bbb;
+                  `}
+                >
+                  — {node.frontmatter.date}
+                </span>
+              </h3>
+              <p>{node.excerpt}</p>
+            </Link>
+          </div>
+        ))}
+      </>
+    )}
+  />
+)
+
+export default Bloglist
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
 import { css } from "@emotion/core"
 import Layout from "../components/layout/layout"
-import { rhythm } from "../utils/typography"
+import Bloglist from "../components/layout/blog-list"
 import SEO from "../components/seo"
 
-export default ({ data }) => {
+export default () => {
   return (
     <Layout>
       <div>
@@ -17,63 +16,8 @@ export default ({ data }) => {
         >
           Blog Top
         </h1>
-        {/* <h4
-          css={css`
-            margin-top: ${rhythm(1)};
-          `}
-        >
-          {data.allMarkdownRemark.totalCount} Posts
-        </h4> */}
-        {data.allMarkdownRemark.edges.map(({ node }) => (
-          <div key={node.id}>
-            <Link
-              to={node.fields.slug}
-              css={css`
-                text-decoration: none;
-                color: inherit;
-              `}
-            >
-              <h3
-                css={css`
-                  // margin-top: ${rhythm(1 / 2)};
-                  margin-bottom: ${rhythm(1 / 4)};
-                `}
-              >
-                {node.frontmatter.title}{" "}
-                <span
-                  css={css`
-                    color: #bbb;
-                  `}
-                >
-                  — {node.frontmatter.date}
-                </span>
-              </h3>
-              <p>{node.excerpt}</p>
-            </Link>
-          </div>
-        ))}
+        <Bloglist />
       </div>
     </Layout>
   )
 }
-
-export const query = graphql`
-  query {
-    allMarkdownRemark(sort: { fields: [frontmatter___date], order: DESC }) {
-      totalCount
-      edges {
-        node {
-          id
-          frontmatter {
-            title
-            date
-          }
-          fields {
-            slug
-          }
-          excerpt
-        }
-      }
-    }
-  }
-`

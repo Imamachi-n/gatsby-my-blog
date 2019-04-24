4dd1e3afce12ed16f1c5ef15d401faf7a4ae70a4
imamachi-n <imamachigatugatu@hotmail.co.jp>
2019-04-24 22:56:17 +0900

    fix ul and ol css styles in markdown files

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,13 +1,26 @@
 import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout/layout"
-// import { css } from "@emotion/core"
+import { css } from "@emotion/core"
 
 export default ({ data }) => {
   const post = data.markdownRemark
   return (
     <Layout>
-      <div>
+      <div
+        css={css`
+          padding-right: 20px;
+          padding-left: 20px;
+          ul,
+          ol {
+            margin-left: 1.44rem;
+            margin-bottom: 1.08rem;
+          }
+          li {
+            margin-bottom: 0.5rem;
+          }
+        `}
+      >
         <h1>{post.frontmatter.title}</h1>
         <div dangerouslySetInnerHTML={{ __html: post.html }} />
       </div>

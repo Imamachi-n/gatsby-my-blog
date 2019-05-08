5d965d3dafafdca8415df285d4b3af6a71d0fc56
imamachi-n <imamachigatugatu@hotmail.co.jp>
2019-05-08 22:43:08 +0900

    create 404 page

--- README.md
+++ README.md
@@ -5,3 +5,5 @@
 ![demo1](./img/demo1.png)
 
 ![demo2](./img/demo2.png)
+
+![demo3](./img/demo3.png)
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,39 @@
 import React from "react"
+import { css } from "@emotion/core"
+import { Browser } from "react-kawaii"
 
 import Layout from "../components/layout/layout"
 import SEO from "../components/seo"
 
+const mainStyle = css`
+  max-width: 1080px;
+  margin: 0 auto;
+  padding: 0 15px;
+  padding-bottom: 15px;
+  h1 {
+    margin: 20px 0 0 0;
+    padding: 0 10px 0 20px;
+  }
+  p {
+    padding: 0 10px 0 20px;
+  }
+`
+
+const svgStyle = css`
+  margin-top: 30px;
+  margin-left: 50px;
+`
+
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div css={mainStyle}>
+      <h1>NOT FOUND...</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <div css={svgStyle}>
+        <Browser size={200} mood="shocked" color="#72ceff" />
+      </div>
+    </div>
   </Layout>
 )
 
